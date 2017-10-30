395955d9ae33695628bfcecf52df737f10fe7198
zpshowhan <1903373681@qq.com>
2017-10-30 17:48:31 +0800

    第一次提交

---
+++ java8code/src/main/java/com/esmart/java8/Lambda.java
@@ -0,0 +1,47 @@
+package com.esmart.java8;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * java8新特性
+ * Lambda 表达式
+ * 允许把函数作为一个方法的参数（函数作为参数传递进方法中）。
+ * 》》》通过实践感觉这个表达式就是适用于接口方法的,这也是java8比较重要的一大更新
+ * @author Thinkpad
+ *
+ */
+public class Lambda {
+
+	public static void main(String[] args) {
+		List<Integer> list=new ArrayList<Integer>();
+		list.add(0);
+		list.add(100);
+		list.add(56);
+		list.add(98);
+		list.add(2);
+		list.add(21);
+		
+		Collections.sort(list, (i1,i2)-> i1.compareTo(i2));
+		
+		//原来的java7是
+		/*Collections.sort(list, new Comparator<Integer>(){
+
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				// TODO Auto-generated method stub
+				
+				return o1.compareTo(o2);
+			}});*/
+		
+		System.out.println("排序后的结合："+list.toString());
+		
+		
+		/**
+		 * 打印结果是
+		 * 排序后的结合：[0, 2, 21, 56, 98, 100]
+		 */
+	}
+}
